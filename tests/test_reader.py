4cac5fac95a768371d2f0284c221622d8ff3e795
Chris Lee-Messer <chris at lee-messer.net>
2018-04-28 20:39:14 -0700

    change tests to use different eeg hdf5 file

--- tests/test_reader.py
+++ tests/test_reader.py
@@ -4,8 +4,8 @@ import numpy as np
 import eeghdf.reader as reader
 
 ROOT = path.dirname(__file__)
-ARFILE1 = path.join(ROOT,r"../notebooks/archive/YA2741BS_1-1+.eeghdf")
-ARFILE2 = path.join(ROOT,r"../notebooks/archive/DA05505C_1-1+.eeghdf")
+ARFILE1 = path.join(ROOT,r"../data/absence_epilepsy.eeghdf")
+ARFILE2 = path.join(ROOT,r"../data/spasms.eeghdf")
 EEGFILE1 = path.normpath(ARFILE1)
 EEGFILE2 = path.normpath(ARFILE2)
 
@@ -19,8 +19,10 @@ def test_reader_open():
 def test_reader_duration():
     eeg = reader.Eeghdf(EEGFILE2)
     dur = eeg.duration_seconds
-    check_val = dur - 3046.0
-    print(dur)
+    calc_dur = eeg.number_samples_per_channel / eeg.sample_frequency
+    check_val = dur - calc_dur # example: 446000/200 = 2230
+
+    print('dur:', dur, 'calc_dur:', calc_dur)
     assert check_val*check_val < 1.0
     
     
@@ -52,8 +54,10 @@ def test_phys_signals():
     eeg.phys_signals[:, x * 200:x * 200 + 10 * 200]
     print(eeg.phys_signals.shape)
     eeg.phys_signals[4, 0:200]
-    assert eeg.phys_signals[3,4] == 0.0
-    assert abs(eeg.phys_signals[3,400] - 50.0 ) < 0.1 # test calibration 50uV
+    print( 'eeg.phys_signals[3,4]:',  eeg.phys_signals[3,4])
+    assert abs(eeg.phys_signals[3,4] - 49.9999973144) < 0.01 
+    print('eeg.phys_signals[3,400]:', eeg.phys_signals[3,400])
+    assert abs(eeg.phys_signals[3,400] ) < 0.1 # test calibration 50uV
 
     eeg.phys_signals[3:5]
 
