beaf853ea8c23f4e52f4adb7796e68f5195e7bcf
Chris Lee-Messer <chris at lee-messer.net>
2018-05-09 21:00:54 -0700

    add test for out of order fancy indexing of phys_channels

--- tests/test_reader.py
+++ tests/test_reader.py
@@ -65,6 +65,10 @@ def test_phys_signals():
     res = eeg.phys_signals[selected_channels,x*200:x*200+200]
     assert res.shape == 3, 200
 
+    selected_channels = [4,1,7] # out of order indexing
+    res = eeg.phys_signals[selected_channels,x*200:x*200+200]
+    assert res.shape == 3, 200
+
 eeg = reader.Eeghdf(EEGFILE1)    
 
 def test_eeghdf_ver2():
