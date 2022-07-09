f1aa19c1f04a2b5a43f43ec19e6f662653701526
Chris Lee-Messer <chris@lee-messer.net>
2022-07-08 22:23:23 -0700

    bump version in setup.py to match __init__.py version

--- setup.py
+++ setup.py
@@ -8,18 +8,20 @@ from distutils.core import setup
 
 setup(
     name="eeghdf",
-    version="0.2",
+    version="0.2.1",
     description="""eeg storage in hdf5 + related functions""",
     author="""Chris Lee-Messer""",
     url="https://github.com/eegml/eeghdf",
     # download_url="",
-    classifiers=["Topic :: Scientific/Engineering :: Medical Science Apps.",
-                 "Development Status :: 4 - Beta",
-                 "Intended Audience :: Science/Research"],
+    classifiers=[
+        "Topic :: Scientific/Engineering :: Medical Science Apps.",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Science/Research",
+    ],
     packages=["eeghdf"],
-    long_description=open('README.md').read(),
+    long_description=open("README.md").read(),
     # package_data={}
     # data_files=[],
     # scripts = [],
-    install_requires = ['numpy', 'h5py', 'pandas', 'future'],
+    install_requires=["numpy", "h5py", "pandas", "future"],
 )
