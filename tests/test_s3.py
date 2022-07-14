a5cbb4c19dc8b93738a0d56a37bcb5b5bec76852
Chris Lee-Messer <chris@lee-messer.net>
2022-07-13 22:09:40 -0700

    update s3 names and add S3_session

--- tests/test_s3.py
+++ tests/test_s3.py
@@ -6,11 +6,11 @@ import eeghdf
 print(settings.to_dict())
 
 
-h5_url_base = settings.GCP_S3_COMPAT_STORAGE_ENDPOINT_URL
+s3_url_endpoint = settings.GCP_S3_COMPAT_STORAGE_ENDPOINT_URL
 bucket_name = settings.BUCKET_NAME
 file_name = r"absence_epilepsy.eeg.h5"
 
-h5_url = osp.join(h5_url_base, f"{bucket_name}/{file_name}")
+h5_url = osp.join(s3_url_endpoint, f"{bucket_name}/{file_name}")
 key_id = settings.GCP_S3_COMPAT_ACCESS_KEY
 key_secret = settings.GCP_S3_COMPAT_SECRET_KEY
 region = settings.S3_REGION
@@ -33,14 +33,24 @@ def test_basic_s3():
 
 
 vfdkwargs = dict(
-    bucket_name=bucket_name,
-    s3_url_endpoint=h5_url_base,
-    secret_id=key_id,
-    secret_key=key_secret,
+    s3_bucket_name=bucket_name,
+    s3_url_endpoint=s3_url_endpoint,
+    access_key_id=key_id,
+    secret_access_key=key_secret,
     region=region,
 )
 
 hf = eeghdf.Eeghdf(file_name, vfd="ros3", vfd_kwargs=vfdkwargs)
 
-print(hf.rawsignals[5,0:10])
-print(hf.phys_signals[0,0:10])
+print(hf.rawsignals[5, 0:10])
+print(hf.phys_signals[0, 0:10])
+
+session = eeghdf.S3_session(
+    s3_bucket_name=bucket_name,
+    s3_url_endpoint=s3_url_endpoint,
+    access_key_id=key_id,
+    secret_access_key=key_secret,
+    region=region,
+)
+f = session.get(file_name)
+print(f.phys_signals[10,1000:1020])
