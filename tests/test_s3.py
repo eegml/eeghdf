2c5ddff624b038e193ed7bda61a06e3b92177ec1
Chris Lee-Messer <chris@lee-messer.net>
2022-07-16 15:43:57 -0700

    make it so S3 tests are skipped if S3 is not configured

--- tests/test_s3.py
+++ tests/test_s3.py
@@ -1,30 +1,42 @@
 from testconfig import settings
+import pytest
 import h5py
 import os.path as osp
 import eeghdf
 
-s3_url_endpoint = settings.S3_COMPAT_STORAGE_ENDPOINT_URL
-bucket_name = settings.S3_BUCKET_NAME
-file_name = r"absence_epilepsy.eeg.h5"
+NOT_CONFIGURED = True
+try:
+    s3_url_endpoint = settings.S3_COMPAT_STORAGE_ENDPOINT_URL
+    bucket_name = settings.S3_BUCKET_NAME
+    file_name = r"absence_epilepsy.eeg.h5"
 
-full_s3_url = osp.join(s3_url_endpoint, f"{bucket_name}/{file_name}")
-key_id = settings.S3_COMPAT_ACCESS_KEY_ID
-key_secret = settings.S3_COMPAT_SECRET_ACCESS_KEY
-region = settings.S3_REGION
+    full_s3_url = osp.join(s3_url_endpoint, f"{bucket_name}/{file_name}")
+    key_id = settings.S3_COMPAT_ACCESS_KEY_ID
+    key_secret = settings.S3_COMPAT_SECRET_ACCESS_KEY
+    region = settings.S3_REGION
+    NOT_CONFIGURED = False
 
-vfdkwargs = dict(
-    s3_bucket_name=bucket_name,
-    s3_url_endpoint=s3_url_endpoint,
-    access_key_id=key_id,
-    secret_access_key=key_secret,
-    region=region,
-)
+
+    vfdkwargs = dict(
+        s3_bucket_name=bucket_name,
+        s3_url_endpoint=s3_url_endpoint,
+        access_key_id=key_id,
+        secret_access_key=key_secret,
+        region=region,
+    )
+
+except:
+    print("S3 not configured, likely need to set up an S3 bucket and create .secrets.toml")
+    pass
 
 
 def test_basic_s3():
     """
     Test the abilty to use h5py files with S3 for our eeghdf files
     """
+    if NOT_CONFIGURED:
+        pytest.skip("S3 not configured")
+        
     h5 = h5py.File(
         full_s3_url,
         driver="ros3",
@@ -40,6 +52,8 @@ def test_basic_s3():
 
 
 def test_s3_direct_vfd():
+    if NOT_CONFIGURED:
+        pytest.skip("S3 not configured")
 
     hf = eeghdf.Eeghdf(file_name, vfd="ros3", vfd_kwargs=vfdkwargs)
 
@@ -47,15 +61,20 @@ def test_s3_direct_vfd():
     print(hf.phys_signals[0, 0:10])
 
 
+# put it outside the function so I can write several tests using same session
 
-session = eeghdf.S3_session(
-    s3_bucket_name=bucket_name,
-    s3_url_endpoint=s3_url_endpoint,
-    access_key_id=key_id,
-    secret_access_key=key_secret,
-    region=region,
-)
+if NOT_CONFIGURED == False:
+    session = eeghdf.S3_session(
+        s3_bucket_name=bucket_name,
+        s3_url_endpoint=s3_url_endpoint,
+        access_key_id=key_id,
+        secret_access_key=key_secret,
+        region=region,
+    )
 
 def test_s3_session():
+    if NOT_CONFIGURED:
+        pytest.skip("S3 not configured")
+
     f = session.get(file_name)
     print(f.phys_signals[10,1000:1020])
